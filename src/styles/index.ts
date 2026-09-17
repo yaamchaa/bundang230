09bc0f2ae9a118ee0896765f4e67e47b62e9c07a
yaamchaa <122985317+luxuryfox@users.noreply.github.com>
2026-09-17 18:49:34 +0900

    restore original CSS styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,7 +1,4 @@
-﻿// CSS imports
-import './fonts.ts'
-import './default_theme.ts'
-import './globals.ts'
-
-// tw-animate-css disabled
-// import 'tw-animate-css'
+﻿import './fonts.css'
+import './default_theme.css'
+import './globals.css'
+import './index.css'
